b8941018d900d17960f4c9f7cb6ed20ba70837c3
Darshan Eshwarappa <darshanjhe@gmail.com>
2025-01-24 17:59:48 +0530

    Tested against other similar html files

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from parser import ArtworkParser
 
 def main():
-    input_file_path = "files/d.html"
+    input_file_path = "files/michelangelo-paintings.html"
     parser = ArtworkParser(input_file_path)
     parser.save_to_json()
 
